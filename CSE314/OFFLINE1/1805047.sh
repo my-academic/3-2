dd0546895b778d6cc660e9c1c8dd7b4862b0ca84
AshfaqRahman <1805047@ugrad.cse.buet.ac.bd>
2022-12-02 17:30:44 +0600

    done offline 1(os)

--- CSE314/OFFLINE1/1805047.sh
+++ CSE314/OFFLINE1/1805047.sh
@@ -16,56 +16,81 @@ for i in $(seq ${max_student_id}); do
     roll_numbers+=("${roll}")
 done
 
-echo "${roll_numbers[@]}" "${marks[@]}"
+# echo "${roll_numbers[@]}" "${marks[@]}"
 
 current_directory=$(pwd)
 accepted_file_name="AcceptedOutput.txt"
 accepted_file=${current_directory}/$accepted_file_name
 
-diff_file_name="diff.txt"
-diff_file=${current_directory}/$diff_file_name
-echo -n "" >$diff_file
-
 submissions=()
 
 cd Submissions
-student_ids=$(ls)
-echo $student_ids
-
-# echo $current_directory
 
 for i in $(ls | sort); do
-    # echo "$i"
+    [[ ! -d "$i" ]] && continue
     cd "$i"
     count=$(ls -1 | grep -c '\.sh')
 
     filename=$(ls -1 | grep '\.sh')
-    # echo $filename ${filename%*.sh}
-    echo -n $count
+
     # 0 => true , 1 => false
-    # (($count != 1)) && echo -n " 0" || echo -n " 1" 
+    # (($count != 1)) && echo -n " 0" || echo -n " 1"
     # [[ ${filename%*.sh} -ne "${i}" ]] && echo -n " 0" || echo -n " 1"
     # [[ ! "${!marks[*]}" =~ "${i}" ]] && echo -n " 0" || echo -n " 1"
     # echo ""
     #  $(($count != 1)) "$()" "$([[ "${!marks[*]}" =~ "${i}" ]])"
     # check if filename is not in array or file and folder name is not same
-    ( (($count != 1)) || [[ ${filename%*.sh} -ne "${i}" ]] || [[ ! "${!marks[*]}" =~ "${i}" ]] ) && cd .. &&  echo "error" && continue
+    ( (($count != 1)) || [[ ${filename%*.sh} -ne "${i}" ]] || [[ ! "${!marks[*]}" =~ "${i}" ]]) && cd .. && continue
 
     output_file=${current_directory}/${i}.txt
     submissions+=("${current_directory}"/Submissions/"$i"/"${filename}")
-    # bash ./${filename} > ${current_directory}/${filename%*.sh}.txt
-    bash ./${filename} >$output_file
+    bash ./${filename} > $output_file
 
-    difference=$(diff $accepted_file $output_file | grep -c ">\|<")
-    # echo "-----------------------------------------" >> "${diff_file}"
-    echo -n "$difference "
+    difference=$(diff -w $accepted_file $output_file | grep -c ">\|<")
+    # echo -n "$difference "
     mark=$(($max_score - $difference * 5))
-    echo $mark
+    (($mark < 0)) && mark=0
+
+    # echo $mark
     marks[$i]=$((marks[$i] + $mark))
+    rm $output_file
 
     cd ..
 
 done
 
-echo "${marks[@]}"
-echo "${submissions[@]}"
+cd ..
+
+# echo "${marks[@]}"
+# echo "${submissions[@]}"
+
+total_submissions=${#submissions[@]}
+
+for i in "${!submissions[@]}"; do
+    path_i=${submissions["$i"]}
+    file_i=${path_i##*/}
+    roll_i=${file_i%.sh}
+    # code $path_i
+    # echo $roll_i
+    for ((j = i + 1; j < "$total_submissions"; j++)); do
+        # (( $i == $j )) && continue
+        path_j=${submissions["$j"]}
+        file_j=${path_j##*/}
+        roll_j=${file_j%.sh}
+        # echo $roll_j
+        count=$(diff -Z -B "$path_i" "$path_j" | grep -c ">\|<")
+        # echo $count
+        (($count == 0)) && ((${marks["$roll_i"]} > 0)) && marks["$roll_i"]=$((-marks["$roll_i"]))
+        (($count == 0)) && ((${marks["$roll_j"]} > 0)) && marks["$roll_j"]=$((-marks["$roll_j"]))
+        # echo $j
+    done
+    # echo "-----------"
+done
+
+# echo "${marks[@]}"
+
+echo "student_id,score" >output.csv
+
+for i in "${!marks[@]}"; do
+    echo "$i","${marks["${i}"]}" >>"output.csv"
+done
