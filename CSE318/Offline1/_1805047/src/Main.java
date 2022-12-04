1197ea0370824590b2ca61e34957f9b0b81dcf09
AshfaqRahman <1805047@ugrad.cse.buet.ac.bd>
2022-12-04 14:08:41 +0600

    final push

--- CSE318/Offline1/_1805047/src/Main.java
+++ CSE318/Offline1/_1805047/src/Main.java
@@ -2,6 +2,32 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+class IntIntArray {
+    private int[][] value;
+
+
+    public IntIntArray(int[][] value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        IntIntArray that = (IntIntArray) o;
+        return Arrays.deepEquals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(value);
+    }
+}
+
 class Board {
     public int[][] arr;
     public int previous_cost;
